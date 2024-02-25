d3e26739fbeef55205b8541c51738cc0f4ef0fce
itsuki <itsuki@localhost>
2024-02-25 14:55:01 +0900

    日報一覧作成完了

--- DailyReportSystemApplication/src/main/java/com/techacademy/controller/ReportController.java
+++ DailyReportSystemApplication/src/main/java/com/techacademy/controller/ReportController.java
@@ -1,11 +1,20 @@
 package com.techacademy.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.techacademy.entity.Employee;
+import com.techacademy.entity.Report;
+import com.techacademy.service.EmployeeService;
 import com.techacademy.service.ReportService;
 
 @Controller
@@ -13,10 +22,12 @@ import com.techacademy.service.ReportService;
 public class ReportController {
 
     private final ReportService reportService;
+    private final EmployeeService employeeService;
 
     @Autowired
-    public ReportController(ReportService reportService) {
+    public ReportController(ReportService reportService, EmployeeService employeeService) {
         this.reportService = reportService;
+        this.employeeService = employeeService;
     }
 
     // 日報一覧画面
@@ -27,5 +38,31 @@ public class ReportController {
         model.addAttribute("reportList", reportService.findAll());
 
         return "reports/list";
+    }
+
+    // 日報新規登録画面
+    @GetMapping(value = "/add")
+    public String create(@ModelAttribute Report report, Employee employee, Model model) {
+
+        // SpringSecurityを使用して、ログイン中のユーザー名を取得
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String loggedInUserID = authentication.getName();
+        Employee loggedInUser = employeeService.findByCode(loggedInUserID);
+        String loggedInUserName = loggedInUser.getName();
+        employee.setName(loggedInUserName);
+
+        return "reports/add";
+    }
+
+    // 日報新規登録処理
+    @PostMapping(value = "/add")
+    public String add(@Validated Report report, BindingResult res, Employee employee, Model model) {
+
+        // 入力チェック
+        if (res.hasErrors()) {
+            return create(report, employee, model);
+        }
+
+        return "redirect:/reports";
     }
 }
--- DailyReportSystemApplication/src/main/java/com/techacademy/service/ReportService.java
+++ DailyReportSystemApplication/src/main/java/com/techacademy/service/ReportService.java
@@ -1,11 +1,14 @@
 package com.techacademy.service;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.techacademy.constants.ErrorKinds;
 import com.techacademy.entity.Report;
 import com.techacademy.repository.ReportRepository;
 
@@ -22,6 +25,20 @@ public class ReportService {
     // 日報一覧表示処理
     public List<Report> findAll() {
         return reportRepository.findAll();
+    }
+
+    // 日報保存
+    @Transactional
+    public ErrorKinds save(Report report) {
+
+        report.setDeleteFlg(false);
+
+        LocalDateTime now = LocalDateTime.now();
+        report.setCreatedAt(now);
+        report.setUpdatedAt(now);
+
+        reportRepository.save(report);
+        return ErrorKinds.SUCCESS;
     }
 
     // 1件を検索
